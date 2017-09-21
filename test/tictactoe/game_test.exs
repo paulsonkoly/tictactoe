1dd3c3b7aaae61a34c757a0c0d3ba3a295181913
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-09-21 19:37:55 +0100

    Added test case for player x winning

--- test/tictactoe/game_test.exs
+++ test/tictactoe/game_test.exs
@@ -6,10 +6,8 @@ defmodule TicTacToe.GameTest do
 
   test "creates an empty board" do
     game = Game.new_game()
-    for x <- 0..2 do
-      for y <- 0..2 do
-        assert game |> Game.query(x, y) == :empty
-      end
+    for x <- 0..2, y <- 0..2 do
+      assert game |> Game.query(x, y) == :empty
     end
   end
 
@@ -19,5 +17,18 @@ defmodule TicTacToe.GameTest do
 
   test "updating the game outside of the board" do
     catch_error (Game.new_game |> Game.update(10,10))
+  end
+
+  test "when a player wins the winner returns that player" do
+    # X O _
+    # X O _
+    # X _ _
+    game = Game.new_game
+           |> Game.update(0, 0)
+           |> Game.update(0, 1)
+           |> Game.update(1, 0)
+           |> Game.update(1, 1)
+           |> Game.update(2, 0)
+    assert Game.winner(game) == :player_x
   end
 end
