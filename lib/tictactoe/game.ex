576d07423901049d6a5c53e95d03355fd242c277
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-09-21 19:07:25 +0100

    All tests passing for Game.
    Still work to do on Game though. Missing: game end and winner
    calculations.

--- lib/tictactoe/game.ex
+++ lib/tictactoe/game.ex
@@ -28,26 +28,46 @@ defmodule TicTacToe.Game do
   ```
   """
 
+  defmodule State do
+    # construct the initial board
+    keys  = for x <- 0..2, y <- 0..2, do: {x, y}
+    board = keys |> Enum.reduce(%{}, fn(k, h) -> Map.put(h, k, :empty) end)
+
+    defstruct next_player: :player_x, board: board
+  end
+
   @doc "Creates a new game with an empty board and `:player_x` to make a move."
   def new_game do
+    %State{}
   end
 
   @doc "Returns the board content on the specified location."
   def query(state, x, y) do
+    state.board[{x, y}]
   end
 
   @doc "Returns the next player to make a move."
   def whos_next(state) do
+    state.next_player
   end
 
   @doc "Returns the winner if there is one. Otherwise returns `:nobody`."
   def winner(state) do
+    :nobody
   end
 
   @doc """
   Makes a move for the player who is next. Raises an error if the move is not
   possible because the position is occupied or the game has ended.
   """
-  def update(state, x, y) do
+  def update(state = %State{ next_player: next, board: board}, x, y) do
+    case who = query(state, x, y) do
+      :empty ->
+        %State{ next_player: opponent(next), board: %{board | {x, y} => next} }
+      _-> raise "position #{x}, #{y} is already occupied by #{who}"
+    end
   end
+
+  defp opponent(:player_x), do: :player_o
+  defp opponent(:player_o), do: :player_x
 end
