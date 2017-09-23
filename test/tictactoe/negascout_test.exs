77a9f33862f5ca39362a639389ebdd9ec77cfae2
Paul Sonkoly <sonkoly.pal@gmail.com>
2017-09-23 10:42:51 +0100

    Two more passing tests to Negascout

--- test/tictactoe/negascout_test.exs
+++ test/tictactoe/negascout_test.exs
@@ -5,4 +5,12 @@ defmodule TicTacToe.NegascoutTest do
   test "negascout stops at depth 0" do
     assert Negascout.negascout(:a, 0, 1, -10, 10) == {3, []}
   end
+
+  test "negascout stops on terminal nodes" do
+    assert Negascout.negascout(:b, 100, 1, -10, 10) == {4, [] }
+  end
+
+  test "negascout reaches one node from the other" do
+    assert Negascout.negascout(:a, 1, 1, -10, 10) == {4, [:a] }
+  end
 end
